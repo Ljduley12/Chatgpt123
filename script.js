56f437ed426e5392494205cf6efca2c7a85c293b
RedNight_Lj <127466421+Ljduley12@users.noreply.github.com>
2023-07-09 22:09:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 // Get DOM elements
 const chatMessages = document.getElementById('chat-messages');
+const historyList = document.getElementById('history-list');
 const userInput = document.getElementById('user-input');
+
+// Chat history array
+const chatHistory = [];
 
 // Event listener for user input
 userInput.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     sendMessage();
+  }
+});
+
+// Event listener for history list
+historyList.addEventListener('click', (event) => {
+  const index = parseInt(event.target.dataset.index);
+  if (!isNaN(index) && index >= 0 && index < chatHistory.length) {
+    userInput.value = chatHistory[index].message;
+    userInput.focus();
   }
 });
 
@@ -16,8 +29,11 @@ function sendMessage() {
   // Process the user message and generate a response
   const botResponse = generateBotResponse(userMessage);
   displayMessage(botResponse, 'bot');
+  // Add the user message to chat history
+  chatHistory.push({ message: userMessage });
   userInput.value = '';
   scrollToBottom();
+  updateHistoryList();
 }
 
 // Function to display a message in the chat container
@@ -32,6 +48,17 @@ function displayMessage(message, sender) {
 function generateBotResponse(userMessage) {
   // Replace this with your logic to generate bot responses
   return 'This is a bot response.';
+}
+
+// Function to update the history list
+function updateHistoryList() {
+  historyList.innerHTML = '';
+  chatHistory.forEach((item, index) => {
+    const listItem = document.createElement('li');
+    listItem.innerText = item.message;
+    listItem.dataset.index = index;
+    historyList.appendChild(listItem);
+  });
 }
 
 // Function to scroll to the bottom of the chat container
